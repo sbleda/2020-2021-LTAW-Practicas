b44336248efe8631782adf1fbeab01c1920cfe53
sbleda <s.bleda.2017@alumnos.urjc.es>
2021-05-16 23:43:31 +0200

    quitando comentarios

--- P1/server.js
+++ P1/server.js
@@ -7,10 +7,8 @@ const PUERTO = 9000;
 http.createServer(function (req, res) {
 
     const url = new URL(req.url, 'http://' + req.headers['host']);
-    console.log(url.pathname);
     tipo= url.pathname.split('.')
     tipo2= url.pathname.split('/')
-    console.log(tipo2)
 
     let file = 'tienda.html'
     let type = "text/html"
@@ -18,49 +16,29 @@ http.createServer(function (req, res) {
     if (tipo[1] == 'css'){
         type = "text/css" 
         file = tipo2[tipo2.length - 2] + '/' + tipo2[tipo2.length - 1]
-        console.log("dentro de css")
-        console.log('')
     } 
 
     else if (tipo[1] == 'png'){
         type = "image/png" 
         file =  tipo2[tipo2.length - 2] + '/' + tipo2[tipo2.length - 1]
-        console.log(file)
-        console.log("dentro de png")
-        console.log('')
     } 
     else if (tipo[1] == 'jpeg'){
         type = "image/jpeg" 
         file = tipo2[tipo2.length - 2] + '/' + tipo2[tipo2.length - 1]
-        console.log(file)
-        console.log("dentro de jpeg")
-        console.log('')
     } 
 
     else if (tipo[1] == 'jpg'){
         type = "image/jpg" 
         file = tipo2[tipo2.length - 2] + '/' + tipo2[tipo2.length - 1]
-        console.log(file)
-        console.log("dentro de jpg")
-        console.log('')
     } 
 
     else if (tipo[1] == 'html'){
         type = "text/html" 
         file = tipo2[tipo2.length - 2] + '/' + tipo2[tipo2.length - 1]
-        console.log(file)
-        console.log("dentro de html")
-        console.log('')
         if ( tipo2[tipo2.length - 1] == 'tienda.html'){
             file = 'tienda.html';
-            console.log(file)
-            console.log("dentro de html bhjbhjhj")
-            console.log('')
-
         }
-
     }
-
 
     fs.readFile(file, function(err, data){
 
@@ -73,12 +51,10 @@ http.createServer(function (req, res) {
             return;
         }
         else {
-
             res.writeHead(200, {'Content-Type': type});
             res.write(data);
             res.end();
         }
-
     });
   }).listen(PUERTO);
   
