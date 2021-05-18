a93622b78082306222d597996a3ba3d032264708
sbleda <s.bleda.2017@alumnos.urjc.es>
2021-05-18 20:31:43 +0200

    EJEMPLOS S8

---
+++ S8/Ejemplos/Ej-04.js
@@ -0,0 +1,26 @@
+//-- Importar express
+const express = require('express');
+
+//-- Crear una aplicación web vacia
+const app = express();
+ 
+//-- Puerto donde lanzar el servidor
+const PORT = 8080;
+
+//-- Definir el punto de entrada principal de mi aplicación web
+app.get('/', function (req, res) {
+  res.send('Bienvenido a mi aplicación Web!!!');
+})
+ 
+//-- Otra vista
+app.get('/woala', (req, res) => {
+    res.send('WOALA! Chuck Norris approved!! :-)');
+    console.log("Acceso a /woala");
+});
+
+//-- Lanzar el servidor
+app.listen(PORT);
+console.log("Servidor Express corriendo en puerto " + PORT);
+
+
+//--Express con dos puntos de entrada
