bcb46a759f551a2077d88e3d6419f5b0c52f6a74
sbleda <s.bleda.2017@alumnos.urjc.es>
2021-05-28 19:49:34 +0200

    opciones mensajes

--- P3/Ej-09-server.js
+++ P3/Ej-09-server.js
@@ -3,6 +3,7 @@ const socket = require('socket.io');
 const http = require('http');
 const express = require('express');
 const colors = require('colors');
+const { Console } = require('console');
 
 const PUERTO = 8080;
 
@@ -28,23 +29,59 @@ app.use('/', express.static(__dirname +'/'));
 //-- El directorio publico contiene ficheros estáticos
 app.use(express.static('public'));
 
+let lista = 0;
 //------------------- GESTION SOCKETS IO
 //-- Evento: Nueva conexion recibida
 io.on('connect', (socket) => {
-  
   console.log('Nuevo usuario conectado'.yellow);
+  lista += 1; 
 
   //-- Evento de desconexión
   socket.on('disconnect', function(){
     console.log('Usuario desconectado'.yellow);
+    lista = lista -1;
   });  
 
   //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
   socket.on("message", (msg)=> {
-    console.log("Mensaje Recibido!: " + msg.blue);
+    msg = msg.split(":");
+    usuario = msg[0];
+    mensaje = msg[1];
+    console.log("Mensaje Recibido" + mensaje.blue);
+
+
+    if (mensaje.trim() == "/help") {
+      ayuda = "/help: Mostrará una lista con todos los comandos soportados" + "<br>" +
+      "/list: Devolverá el número de usuarios conectados" + "<br>" + 
+      "/hello: El servidor nos devolverá el saludo" + "<br>" +
+      "/date: Nos devolverá la fecha "
+      socket.send(ayuda);
+    }
+    else if (mensaje.trim() == "/list") {
+      socket.send(lista);
+    }
+    else if (mensaje.trim() == "/hello") {
+      socket.send("Hola");
+    }
+    else if (mensaje.trim() == "/date") {
+        var date = new Date();  
+        var hora = date.getHours();
+        hour = (hora < 10 ? "0" : "") + hora; 
+        var min  = date.getMinutes();
+        min = (min < 10 ? "0" : "") + min;
+        var seg  = date.getSeconds();
+        seg = (seg < 10 ? "0" : "") + seg;
+        var year = date.getFullYear();  
+        var mes = date.getMonth() + 1;
+        mes = (mes < 10 ? "0" : "") + mes;
+        var dia  = date.getDate();
+        dia = (dia < 10 ? "0" : "") + dia;
+        socket.send("Fecha: " + dia + "/" + mes + "/" + year + "<br>" +  "Hora: " + hora + ":" + min + ":" + seg);    }
+    else{
+      io.send(usuario + ":" + mensaje);
+    }
 
     //-- Reenviarlo a todos los clientes conectados
-    io.send(msg);
   });
 
 });
