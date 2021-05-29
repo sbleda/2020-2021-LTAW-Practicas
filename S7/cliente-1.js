db3a497de5338efd7df7f2a5a812815085ec3fc9
sbleda <s.bleda.2017@alumnos.urjc.es>
2021-05-29 13:40:41 +0200

    ejercicios

--- S7/cliente-1.js
+++
@@ -1,8 +0,0 @@
-console.log("Ejecutando Javascript...");
-
-const display = document.getElementById("display");
-const boton_test = document.getElementById("boton_test");
-
-boton_test.onclick = ()=> {
-    display.innerHTML+="<p>Hola desde JS!</p>";
-}
