e09d5f5171f8335b3cb41059544308c2c953e6d8
sbleda <s.bleda.2017@alumnos.urjc.es>
2021-05-29 14:02:32 +0200

    s10

--- S10/main.js
+++ S10/main.js
@@ -1,3 +1,8 @@
 const electron = require('electron');
 
 console.log("Arrancando electron...");
+//-- Punto de entrada. En cuanto electron está listo,
+//-- ejecuta esta función
+electron.app.on('ready', ()=>{
+    console.log("Evento Ready!")
+});
